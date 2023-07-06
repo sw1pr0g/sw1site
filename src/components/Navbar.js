afdd38fbee07b9bd2d3395efbfdb0666ab6f9ea2
SW1pr0g <poryadin_alex@mail.ru>
2023-07-06 14:19:44 +0300

    Add a basic routing

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="toggleButton">
+                <button></button>
+            </div>
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/experience">Experience</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
