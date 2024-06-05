93358e0d707f73dab2e8c43bb743693932455141
SW1pr0g <poryadin_alex@mail.ru>
2024-06-06 01:18:23 +0300

    add wasm files

--- internal/components/home.go
+++ internal/components/home.go
@@ -1,11 +1,20 @@
 package components
 
-import "github.com/maxence-charriere/go-app/v9/pkg/app"
+import (
+	"github.com/maxence-charriere/go-app/v9/pkg/app"
+)
 
 type Home struct {
 	app.Compo
 }
 
 func (h *Home) Render() app.UI {
-	return app.Div().Text("Home Page")
+	return app.Div().Body(
+		app.H1().
+			Class("title").
+			Text("Build a GUI with Go"),
+		app.P().
+			Class("text").
+			Text("Just because Go and this package are really awesome!"),
+	)
 }
