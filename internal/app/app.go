bc691ecb21b4ca2051cd9e1baa0e6370242333e9
SW1pr0g <poryadin_alex@mail.ru>
2024-06-09 23:49:28 +0300

    add base webassembly files

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,51 +3,30 @@ package app
 import (
 	"github.com/maxence-charriere/go-app/v9/pkg/app"
 	"github.com/sw1pr0g/sw1site/config"
+	"github.com/sw1pr0g/sw1site/internal/components"
 	"log"
+	"mime"
 	"net/http"
 )
 
-// Run запускает приложение с заданной конфигурацией
 func Run(cfg *config.Config) {
-	// Регистрация маршрутов приложения
-	app.Route("/", &home{})
-	app.Route("/profile", &profile{})
+	app.Route("/", &components.Home{})
 	app.RunWhenOnBrowser()
 
-	// Настройка сервера для обслуживания статических файлов
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	if err := mime.AddExtensionType(".wasm", "application/wasm"); err != nil {
+		log.Fatal(err)
+	}
 
-	// Настройка основного приложения
+	staticDir := "/static/web"
+	fs := http.FileServer(http.Dir(staticDir))
+	http.Handle("/static/web/", http.StripPrefix("/static/web/", fs))
+
 	http.Handle("/", &app.Handler{
-		Name:        cfg.App.Name,
-		Description: "An example",
+		Name: cfg.App.Name,
 	})
 
-	log.Printf("Starting server on :%s", cfg.HTTP.Port)
+	log.Printf("Starting server on http://localhost:%s", cfg.HTTP.Port)
 	if err := http.ListenAndServe(":"+cfg.HTTP.Port, nil); err != nil {
 		log.Fatal(err)
 	}
-}
-
-type home struct {
-	app.Compo
-}
-
-func (h *home) Render() app.UI {
-	return app.Div().Body(
-		app.H1().Text("Hello, World!"),
-		app.P().Text("This is a home page."),
-	)
-}
-
-type profile struct {
-	app.Compo
-}
-
-func (p *profile) Render() app.UI {
-	return app.Div().Body(
-		app.H1().Text("Profile Page"),
-		app.P().Text("This is a profile page."),
-	)
 }
